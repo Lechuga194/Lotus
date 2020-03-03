87ce1733d52674cf8bc7065249237349d0dd4266
Lechuga194 <joselechuga194@gmail.com>
2020-03-03 00:16:10 -0600

    practica03

---
+++ Lotus/music/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Bienvenido al servicio de Streaming Lotus!")
+
+
+def top_songs(request):
+    context = {}
+    return render(request, 'music/index.html', context)
